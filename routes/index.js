4cbe55965aa0611d3e2d599f85950e1a392c4ebc
Mohamed Alaga <malaga.142014@gmail.com>
2024-12-11 11:05:30 +0200

    2. First API

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ import AppController from '../controllers/AppController';
 
 const router = express.Router();
 
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
+const routeController = (app) => {
+  app.use('/', router);
+  router.get('/status', AppController.getStatus);
+  router.get('/stats', AppController.getStats);
+};
 
-export default router;
+export default routeController;
--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 import express from 'express';
-import routes from './routes/index';
+import routeController from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use('/', routes);
-
-app.use((err, req, res) => {
-  console.error(err.stack);
-  res.status(500).send({ error: 'Something went wrong!' });
-});
+routeController(app);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
