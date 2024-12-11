59268c49bb4a53dad63be711415c8e5de0116131
Mohamed Alaga <malaga.142014@gmail.com>
2024-12-11 08:52:56 +0200

    1. MongoDB utils

---
+++ utils/db.js
@@ -0,0 +1,28 @@
+import mongo from 'mongodb';
+
+class DBClient {
+  constructor() {
+    this.host = process.env.DB_HOST || 'localhost';
+    this.port = process.env.DB_PORT || 27017;
+    this.db = mongo.MongoClient.connect(`${this.host}:${this.port}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }).then((client) => client.db(process.env.DB_DATABASE || 'files_manager'));
+  }
+
+  isAlive() {
+    return this.db.then((db) => db.command({ ping: 1 }).then(() => true));
+  }
+
+  nbUsers() {
+    return this.db.then((db) => db.collection('users').countDocuments());
+  }
+
+  nbFiles() {
+    return this.db.then((db) => db.collection('files').countDocuments());
+  }
+}
+
+const dbClient = new DBClient();
+
+export default dbClient;
