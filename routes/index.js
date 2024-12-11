51dbd41f561d20d5866ead369be84dcb3d7a2974
Mohamed Alaga <malaga.142014@gmail.com>
2024-12-11 11:19:11 +0200

    2. First API

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,9 @@
-import express from 'express';
+import { Router } from 'express';
 import AppController from '../controllers/AppController';
 
-const router = express.Router();
+const router = Router();
 
-const routeController = (app) => {
-  app.use('/', router);
-  router.get('/status', (req, res) => {
-    AppController.getStatus(req, res);
-  });
-  router.get('/stats', (req, res) => {
-    AppController.getStats(req, res);
-  });
-};
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
 
-export default routeController;
+export default router;
