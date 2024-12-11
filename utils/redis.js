506fb411fd4df22a771fd6a5511455efd7ba49f5
Mohamed Alaga <malaga.142014@gmail.com>
2024-12-11 08:05:20 +0200

    0. Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,30 @@
+import redis from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient().on('error', (err) => {
+      console.log(`Redis client not connected to the server: ${err.message}`);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async set(key, value, duration) {
+    this.client.set(key, value);
+    this.client.expire(key, duration);
+  }
+
+  async get(key) {
+    const value = await this.client.get(key);
+    return value;
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
