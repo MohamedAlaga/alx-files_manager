9baf8fd3bd79f40491a79905ddd569027b2b7895
Mohamed Alaga <malaga.142014@gmail.com>
2024-12-12 04:38:34 +0200

    fixes

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,51 +1,56 @@
-/* eslint-disable consistent-return */
-import { ObjectId } from 'mongodb';
-
 import sha1 from 'sha1';
+import Queue from 'bull/lib/queue';
 import dbClient from '../utils/db';
-import redisClient from '../utils/redis';
 
+const userQueue = new Queue('email sending');
+
+/**
+ * Controller for the index route.
+ * @class UsersController
+ * @method postNew
+ * @method getMe
+ */
 class UsersController {
-  static async postNew(request, response) {
+  /**
+   * Method for the route POST /users.
+   * Create a new user in DB.
+   * @param {object} req - The express request object.
+   * @param {object} res - The express response object.
+   * @returns {object} The status code 201 and the new user if successful,
+   *                   400 if missing parameters or 409 if already exist.
+   */
+  static async postNew(req, res) {
+    const email = req.body ? req.body.email : null;
+    const password = req.body ? req.body.password : null;
+
+    if (!email) return res.status(400).json({ error: 'Missing email' });
+    if (!password) return res.status(400).json({ error: 'Missing password' });
+
+    const user = await (await dbClient.usersCollection()).findOne({ email });
+
+    if (user) return res.status(400).json({ error: 'Already exist' });
+
     try {
-      const { email, password } = request.body;
-      if (!email) return response.status(400).send({ error: 'Missing email' });
-      if (!password) return response.status(400).send({ error: 'Missing password' });
-      const collection = dbClient.db.collection('users');
-      const isExist = await collection.findOne({ email });
-      if (isExist) return response.status(400).send({ error: 'Already exist' });
-      const hashPass = sha1(password);
-      collection.insertOne({ email, hashPass });
-      const userid = await collection.findOne(
-        { email }, { projection: { email: 1 } },
-      );
-      return response.status(201).send({ email, userid });
-    } catch (error) {
-      return response
-        .status(500)
-        .send({
-          error: `Internal Server Error${error}`,
-          isAlive: dbClient.isAlive(),
-          files: await dbClient.nbFiles(),
-          users: await dbClient.nbUsers(),
-        });
+      const addUserInfo = await (await dbClient.usersCollection())
+        .insertOne({ email, password: sha1(password) });
+      const userId = addUserInfo.insertedId.toString();
+
+      userQueue.add({ userId });
+      return res.status(201).json({ id: userId, email });
+    } catch (err) {
+      return res.status(500).json({ error: err.message });
     }
   }
 
+  /**
+   * Method for the route GET /users/me.
+   * Fetches a user in DB.
+   * @param {object} req - The express request object.
+   * @param {object} res - The express response object.
+   * @returns {object} The status code 200 and the user if successful,
+   */
   static async getMe(req, res) {
-    try {
-      const userToken = req.header('X-Token');
-      const authKey = `auth_${userToken}`;
-      const userID = await redisClient.get(authKey);
-      if (!userID) {
-        res.status(401).json({ error: 'Unauthorized' });
-      }
-      const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(userID) });
-      res.json({ id: user._id, email: user.email });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: `Server error :${error}` });
-    }
+    return res.status(200).json({ id: req.user._id.toString(), email: req.user.email });
   }
 }
 
